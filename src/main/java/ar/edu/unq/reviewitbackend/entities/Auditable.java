b8e8dbf2d91b119acc465f7265ba19fb889bfa83
mcastello <mcastello@nacionservicios.com.ar>
2021-04-29 12:43:29 -0300

    Fix auditable

--- src/main/java/ar/edu/unq/reviewitbackend/entities/Auditable.java
+++ src/main/java/ar/edu/unq/reviewitbackend/entities/Auditable.java
@@ -10,15 +10,20 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public class Auditable{
 
     @CreatedDate
     @Column(name = "created_date", updatable = false)
-    private Date createdDate;
+    protected Date createdDate;
 
     @LastModifiedDate
     @Column(name = "last_modified_date")
-    private Date lastModifiedDate;
+    protected Date lastModifiedDate;
 }
