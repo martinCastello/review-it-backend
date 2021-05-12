59ee109ab3b3557de07c49e4cd792eb7f895d922
Mariel Lopez <mariel.lopez@d3sistemas.com.ar>
2021-05-12 19:15:48 -0300

    Se eliminaron anotation para solucionar tema con travis

--- src/main/java/ar/edu/unq/reviewitbackend/utils/OrderBy.java
+++ src/main/java/ar/edu/unq/reviewitbackend/utils/OrderBy.java
@@ -1,14 +1,6 @@
 package ar.edu.unq.reviewitbackend.utils;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
 public class OrderBy {
 	
 	String name;
