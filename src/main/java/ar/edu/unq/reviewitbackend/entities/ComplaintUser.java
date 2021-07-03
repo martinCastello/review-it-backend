6b3d6ef6deb3d4063d5644b6ebe4d04e4ba8b732
mcastello <mcastello@nacionservicios.com.ar>
2021-07-03 16:28:49 -0300

    .

--- src/main/java/ar/edu/unq/reviewitbackend/entities/ComplaintUser.java
+++ src/main/java/ar/edu/unq/reviewitbackend/entities/ComplaintUser.java
@@ -6,7 +6,6 @@ import javax.persistence.Table;
 import javax.persistence.Transient;
 import javax.validation.constraints.NotNull;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 @Entity
@@ -15,7 +14,6 @@ public class ComplaintUser extends Complaint{
 
 	@ManyToOne
 	@JsonProperty(access = JsonProperty.Access.READ_ONLY)
-	@JsonIgnore
 	private User to;
 	
 	@NotNull
