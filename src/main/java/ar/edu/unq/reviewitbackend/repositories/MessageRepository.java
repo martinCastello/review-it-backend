7d53199c363598e889acc23f3ae84e09b8ef70d2
Mariel Lopez <mariel.lopez@d3sistemas.com.ar>
2021-07-04 18:14:41 -0300

    Se parametrizó metodo que trae mensajes entre 2 usuarios

--- src/main/java/ar/edu/unq/reviewitbackend/repositories/MessageRepository.java
+++ src/main/java/ar/edu/unq/reviewitbackend/repositories/MessageRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import ar.edu.unq.reviewitbackend.entities.Message;
@@ -15,6 +16,6 @@ public interface MessageRepository extends JpaRepository<Message, Long>{
 	
     List<Message> findAllByFrom(User user);
     
- 	@Query("SELECT m FROM Message m WHERE (m.from = 1 AND m.to = 2) OR (m.to = 1 AND m.from= 2) ORDER BY m.id ASC")
-	List<Message> findByFromAndTo(Long userFrom, Long userTo);
+ 	@Query("SELECT m FROM Message m WHERE (m.from = (:userFrom) AND m.to = (:userTo)) OR (m.to = 2 AND m.from= 1) ORDER BY m.id ASC")
+	List<Message> findByFromAndTo(@Param("userFrom") User userFrom, @Param("userTo") User userTo);
 }
